0e0733e144924ca2529761e0a1d36df5e08e4638
PixelSkider <MingLoL@foxmail.com>
2024-08-04 15:46:49 +0800

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -1,58 +1,78 @@
+let clickCount = 0;
+let intervalId;
+
 document.addEventListener("click", function (e) {
+  clickCount++;
   createMessage(e.pageX, e.pageY);
+
+  if (clickCount === 15) {
+    startAutoMessages();
+  }
 });
-
+// 轻小说 破坏掉的世界
 function createMessage(x, y) {
-  const messages = [
-    // KALUDDDDDDDDDDDD
-    "晨雾中消溶的森林",
-    "牛乳的污渍",
-    "绚丽的落英",
-    "风之结晶",
-    "墙挂的鲱鱼",
-    "倒立的刺猬",
-    "袋中的脏器",
-    "上锁的红色魔法书",
-    "甜蜜至骨的死期",
-    "徘徊在暮色中的无头小丑",
-    "厚重的绘本",
-    "温暖的旋律",
-    "黑白交错的锯齿线",
-    "摇拽的小船",
-    "受创的茧",
-    "红色的水",
-    "灰色之风",
-    "七只眼的青蛙",
-    "无尽的橙色道路",
-    "银色闪耀的布料",
-    "吞下蟒蛇的狮子",
-    "圆形的彩虹",
-    "堆积成山的金币",
-    "小碗里的糖果",
-    "燃烧旺盛的路标",
-    "在星河里游泳的鱼",
-    "红发少女",
-    "燃烧着掉落着的无数的书",
-    "火焰中游动的蛇",
-    "缺陷崩溃的银色的月亮",
-    "自相残杀的姐妹公主",
-    "死者说出谎言",
-    "搏动的大铃",
-    "从天倾注的牛奶柱",
-    "空中舞动的绸带",
-  ];
-  const colors = ["#FF0000", "#FF3030", "#FF4040", "#FF0000", "#8B0000"];
+  fetch("../resource/zh-hans/Messages.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const messages = data.messages;
+      const colors = data.colors;
 
-  const message = document.createElement("div");
-  message.className = "message";
-  message.textContent = messages[Math.floor(Math.random() * messages.length)];
-  message.style.left = `${x}px`;
-  message.style.top = `${y}px`;
-  message.style.color = colors[Math.floor(Math.random() * colors.length)];
+      const message = document.createElement("div");
+      message.className = "message";
+      message.textContent =
+        messages[Math.floor(Math.random() * messages.length)];
+      message.style.position = "absolute";
+      message.style.color = colors[Math.floor(Math.random() * colors.length)];
 
-  document.body.appendChild(message);
+      document.body.appendChild(message);
 
-  setTimeout(() => {
-    message.remove();
-  }, 2000);
+      // 获取元素的宽度和高度
+      const messageWidth = message.offsetWidth;
+      const messageHeight = message.offsetHeight;
+
+      // 确保消息在视窗内显示
+      if (x + messageWidth > window.innerWidth) {
+        x = window.innerWidth - messageWidth;
+      }
+      if (y + messageHeight > window.innerHeight) {
+        y = window.innerHeight - messageHeight;
+      }
+
+      message.style.left = `${x}px`;
+      message.style.top = `${y}px`;
+
+      setTimeout(() => {
+        message.remove();
+      }, 2000);
+    })
+    .catch((error) => console.error("Error fetching the messages:", error));
+}
+
+function startAutoMessages() {
+  intervalId = setInterval(() => {
+    const x = Math.random() * window.innerWidth;
+    const y = Math.random() * window.innerHeight;
+    createMessage(x, y);
+  }, 300);
+}
+
+function remindURL(url, language = "zh-CN") {
+  let confirmMessage;
+  if (language === "zh-CN" || language === null) {
+    confirmMessage =
+      "你确定要跳转到以下链接吗? (失效可反馈 侵权删)\n(对于任何通过本网站链接到的第三方网站，我们不对其内容负责。使用本网站即表示您同意以上免责声明)\n" +
+      url;
+  } else if (language === "en-US") {
+    confirmMessage =
+      "Are you sure you want to jump to the following link? (Invalid can be fed back and deleted for infringement)\n(We are not responsible for the content of any third-party websites linked through this website. By using this website, you agree to the above disclaimer)\n" +
+      url;
+  }
+  if (confirm(confirmMessage)) {
+    window.open(url, "_blank");
+  }
+}
+
+function remindText(text) {
+  const confirmMessage = text;
+  confirm(confirmMessage);
 }
